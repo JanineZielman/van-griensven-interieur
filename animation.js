b5462e8361a60a7149860e3e2e41eb6b0c46e104
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-12-18 19:14:04 +0100

    update

--- animation.js
+++ animation.js
@@ -5,7 +5,7 @@ let value1_2 = 45.95;
 
 let value2_0 = 121.7;
 let value2_1 = 92.21;
-let value2_2 = 161.59;
+let value2_2 = 89.09;
 
 let value3_0 = 72.28;
 let value3_1 = 122.32;
@@ -19,9 +19,9 @@ const ranges = {
     value1_2: [30, 50]
   },
   polygon2: {
-    value2_0: [120, 125],
-    value2_1: [70, 95],
-    value2_2: [140, 160]
+    value2_0: [100, 145],
+    value2_1: [92.21, 92.21],
+    value2_2: [60, 90]
   },
   polygon3: {
     value3_0: [30, 80],
@@ -48,7 +48,7 @@ function updatePolygons() {
   const polygon3 = document.getElementById("polygon3");
 
   const points1 = `0 29.67 0 156.25 20.54 181.83 97.96 181.83 ${value1_0} ${value1_1} ${value1_2} 114.5 0 29.67`;
-  const points2 = `161.59 128.98 163.43 182.54 101.76 182.54 102.67 133.69 48.81 111.52 37.6 89.09 ${value2_0} ${value2_1} ${value2_2} 128.98`;
+  const points2 = `161.59 128.98 163.43 182.54 101.76 182.54 102.67 133.69 48.81 111.52 37.6 ${value2_2} ${value2_0} ${value2_1} 161.59 128.98`;
   const points3 = `16.42 3.75 2.02 24.62 28.88 ${value3_0} ${value3_1} ${value3_2} 133.53 4.37 16.42 3.75`;
 
   polygon1.setAttribute("points", points1);
